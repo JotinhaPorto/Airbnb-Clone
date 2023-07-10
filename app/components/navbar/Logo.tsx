7dc0bed3e8a689a1ec5b8b07e03efcc9d8f67986
JotinhaPorto <122660604+JotinhaPorto@users.noreply.github.com>
2023-07-10 13:55:00 -0300

    segundo commit

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+
+const Logo = () => {
+
+    const router = useRouter()
+
+    return (
+        <Image
+            onClick={() => router.push('/')}
+            alt='logo'
+            src="/images/logo.png"
+            height={100}
+            width={100}
+            className='hidden md:block cursor-pointer'
+        />
+    )
+}
+
+export default Logo
